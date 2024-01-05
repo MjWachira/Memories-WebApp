7ea3d998884564da61a44102a75be089a863609f
Johnny <mjwachira1@gmail.com>
2024-01-05 12:09:38 +0300

    Add project files.

---
+++ MemoryApp/Models/Memory.cs
@@ -0,0 +1,11 @@
+﻿namespace MemoryApp.Models
+{
+    public class Memory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; } =DateTime.Now;
+        public string location { get; set; } = string.Empty;
+    }
+}
